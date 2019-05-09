2434720e710743fc0cea349572e3d7d0e48a3aa1
Martin Hesslund <martin.hesslund@callistaenterprise.se>
2019-05-09 17:23:48 +0200

    BUILDENV: Fixat license och hur cypress rapporterar fel

--- test/scripts/run.js
+++ test/scripts/run.js
@@ -31,15 +31,19 @@ const cypressOption = {
 };
 
 cypress.run(cypressOption).then(
-  () => {
-    generateReport(options)
-  },
-  error => {
-    generateReport(options);
-    console.error(error);
+  (results) => {
+  //console.log(results);
+  generateReport(options);
+
+  if (results.totalFailed > 0) {
     process.exit(1)
   }
-);
+}
+).catch(error => {
+    generateReport(options);
+  console.error(error);
+  process.exit(1)
+});
 
 function generateReport(options) {
     return merge(options).then(report => marge.create(report, options))
