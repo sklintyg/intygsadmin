38406c4c7b16b4039efd70dfff94cca297bb4ed0
Linus Forsner <linus.forsner@gmail.com>
2019-05-23 10:24:49 +0200

    INTYGFV-11640: Validering för datum och tid, Brutit ut helpchevron.

--- web/client/src/components/datePicker/DatePicker.js
+++ web/client/src/components/datePicker/DatePicker.js
@@ -1,11 +1,10 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import ReactDatePicker from 'react-datepicker'
 import 'react-datepicker/dist/react-datepicker.css'
-import sv from 'date-fns/locale/sv'
 import styled from 'styled-components'
 import { Button } from 'reactstrap'
 import { Calendar } from '../styles/iaSvgIcons'
-
+import colors from '../styles/iaColors'
 
 const StyledButton = styled(Button)`
   margin-left: 0px !important
@@ -19,19 +18,57 @@ const StyledInput = styled.input`
   width: 100px !important;
 `
 
+const Container = styled.div`
+  &.error {
+    > input,
+    > input:focus,
+    > button,
+    > button:focus {
+      border-color: ${colors.IA_COLOR_16};
+    }
+  }
+`
+
 class DatePickerInput extends React.Component {
   render() {
     return (
-      <>
-        <StyledInput type="text" value={this.props.value} onChange={this.props.onChange} placeholder={'åååå-mm-dd'}/>
-        <StyledButton onClick={this.props.onClick} color={'default'}><Calendar/></StyledButton>
-      </>
+      <Container className={this.props.className}>
+        <StyledInput
+          type="text"
+          value={this.props.value}
+          onChange={this.props.onChange}
+          placeholder={'åååå-mm-dd'}
+        />
+        <StyledButton onClick={this.props.onClick} color={'default'}>
+          <Calendar />
+        </StyledButton>
+      </Container>
     )
   }
 }
 
-const DatePicker = ({date, onChange}) => {
-  return <ReactDatePicker customInput={<DatePickerInput />} selected={date} locale={sv} dateFormat={'yyyy-MM-dd'} onChange={onChange}/>
+const DatePicker = ({ date, onChange, className}) => {
+  const [internalDate, setInternalDate] = useState(undefined)
+  const change = (event) => {
+    onChange(event.target.value)
+  }
+
+  useEffect(() => {
+    console.log(date)
+    if (date && date.getHours) {
+      setInternalDate(date)
+    }
+  }, [date])
+  return (
+    <ReactDatePicker
+      customInput={<DatePickerInput />}
+      selected={internalDate}
+      dateFormat={'yyyy-MM-dd'}
+      onChange={onChange}
+      onChangeRaw={change}
+      className={className}
+    />
+  )
 }
 
 export default DatePicker
---
+++ web/client/src/components/helpChevron/HelpChevron.js
@@ -0,0 +1,66 @@
+import React, { useState } from 'react'
+import colors from '../styles/iaColors'
+import Toggler from '../toggler/Toggler'
+import styled from 'styled-components'
+
+const HelpDiv = styled.div`
+  &.visible {
+    display: block;
+  }
+  &.hidden {
+    display: none;
+  }
+
+  border-radius: 2px;
+  background-color: ${colors.IA_COLOR_15};
+  max-height: 160px;
+  padding: 12px 18px;
+  overflow-y: scroll;
+  margin-bottom: 12px;
+  h5 {
+    color: ${colors.IA_COLOR_06}
+    padding: 4px 0;
+  }
+  p {
+    color: ${colors.IA_COLOR_06}
+    margin: 0;
+  }
+`
+
+const HelpRoof = styled.div`
+  margin-top: -25px;
+  &.visible {
+    display: block;
+  }
+  &.hidden {
+    display: none;
+  }
+  height: 20px;
+  width: 20px;
+  border: 10px solid ${colors.IA_COLOR_15};
+  margin-left: 20px;
+  border-top: 10px solid #fff;
+  border-left: 10px solid #fff;
+  border-right: 10px solid #fff;
+`
+
+const HelpChevron = ({ label, children }) => {
+  const [prioHelpExpanded, setPrioHelpExpanded] = useState(false)
+  const prioHelpToggler = () => {
+    setPrioHelpExpanded(!prioHelpExpanded)
+  }
+  return (
+    <>
+      <h5>
+        {label}
+        <Toggler expanded={prioHelpExpanded} handleToggle={prioHelpToggler} />
+      </h5>
+      <HelpRoof className={prioHelpExpanded ? 'visible' : 'hidden'} />
+      <HelpDiv className={prioHelpExpanded ? 'visible' : 'hidden'}>
+        {children}
+      </HelpDiv>
+    </>
+  )
+}
+
+export default HelpChevron
