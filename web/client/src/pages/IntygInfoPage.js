4eaa14e05d2a613f4f092b376baccfe1b434ce38
Martin Hesslund <martin.hesslund@callistaenterprise.se>
2019-09-25 13:14:14 +0200

    INTYGFV-12107: Fixat miss vid tidigare rättninge

--- web/client/src/pages/IntygInfoPage.js
+++ web/client/src/pages/IntygInfoPage.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import { FlexColumnContainer, CustomScrollingContainer, PageContainer } from '../components/styles/iaLayout'
 import MenuBar from '../components/iaMenu/MenuBar';
-import IntygInfoPageHeader from "../components/inygInfo/IntygInfoPageHeader";
-import IntygInfoSearch from "../components/inygInfo/IntygInfoSearch";
-import IntygInfoHistory from "../components/inygInfo/IntygInfoHistory";
+import IntygInfoPageHeader from "../components/intygInfo/IntygInfoPageHeader";
+import IntygInfoSearch from "../components/intygInfo/IntygInfoSearch";
+import IntygInfoHistory from "../components/intygInfo/IntygInfoHistory";
 
 const IntygInfoPage = () => {
   return (
