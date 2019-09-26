9a98701b0b5f0476c7889379c2ec046be8a7d697
Martin Hesslund <martin.hesslund@callistaenterprise.se>
2019-09-26 13:56:22 +0200

    INTYGFV-12107: Uppdaterat texterna

--- web/client/src/components/intygInfo/IntygInfoDialog.js
+++ web/client/src/components/intygInfo/IntygInfoDialog.js
@@ -1,7 +1,7 @@
 import React from 'react'
-import {Modal, ModalBody, ModalHeader, Button, ModalFooter, Table} from 'reactstrap'
+import {Button, Modal, ModalBody, ModalFooter, ModalHeader, Table} from 'reactstrap'
 import modalContainer from '../modalContainer/modalContainer'
-import { compose } from 'recompose'
+import {compose} from 'recompose'
 import {IaTypo02, IaTypo03, IaTypo05} from "../styles/iaTypography";
 import colors from "../styles/iaColors";
 import DisplayDateTime from "../displayDateTime/DisplayDateTime";
@@ -180,110 +180,104 @@ const IntygEventRow = ({event, fetchIntygInfo}) => {
   )
 }
 
-const IntygInfoDialog = ({ handleClose, isOpen, data, fetchIntygInfo }) => {
+const IntygInfoDialog = ({handleClose, isOpen, data, fetchIntygInfo}) => {
 
   if (!data) {
     return null
   }
 
-  const { intygInfo } = data
+  const {intygInfo} = data
 
   return (
-      <Modal id={'intygInfoDialogId'} isOpen={isOpen} size={'xl'} backdrop={true} toggle={handleClose}>
-        <ModalHeader toggle={handleClose}>Intygsinformation för ID: {intygInfo.intygId}</ModalHeader>
-        <ModalBody>
+    <Modal id={'intygInfoDialogId'} isOpen={isOpen} size={'xl'} backdrop={true} toggle={handleClose}>
+      <ModalHeader toggle={handleClose}>Intygsinformation för ID: {intygInfo.intygId}</ModalHeader>
+      <ModalBody>
 
-          <RowWrapper>
-            <IaTypo03 color={colors.IA_COLOR_01}>
-              {intygInfo.intygType}
-              <IntygVersion>
-                {intygInfo.intygVersion}
-              </IntygVersion>
-            </IaTypo03>
-          </RowWrapper>
+        <RowWrapper>
+          <IaTypo03 color={colors.IA_COLOR_01}>
+            {intygInfo.intygType}
+            <IntygVersion>
+              {intygInfo.intygVersion}
+            </IntygVersion>
+          </IaTypo03>
+        </RowWrapper>
 
-          <RowWrapperBox>
-            <InfoBox title='Utkastet skapades' value={intygInfo.draftCreated} noValue='Okänt' />
-            <InfoBox title='Intyget signerades' value={intygInfo.signedDate} noValue='Ej signerat' />
-            <InfoBox title='Intygstjänsten tog emot intyget' value={intygInfo.receivedDate} noValue='Ej mottagit' />
-            <InfoBox
-              title='Intyg skickades till intygsmottagaren'
-              value={intygInfo.sentToRecipient}
-              noValue={intygInfo.numberOfRecipients  > 0 ? 'Ej skickat' : 'Finns ingen mottagare'} />
-          </RowWrapperBox>
+        <RowWrapperBox>
+          <InfoBox title='Utkastet skapades' value={intygInfo.draftCreated} noValue='Okänt' />
+          <InfoBox title='Intyget signerades' value={intygInfo.signedDate} noValue='Ej signerat' />
+          <InfoBox title='Intygstjänsten tog emot intyget' value={intygInfo.receivedDate} noValue='Ej mottagit' />
+          <InfoBox title='Intyget skickades till intygsmottagaren'
+                   value={intygInfo.sentToRecipient}
+                   noValue={intygInfo.numberOfRecipients > 0 ? 'Ej skickat' : 'Finns ingen mottagare'} />
+        </RowWrapperBox>
 
-          <RowWrapper>
-            <IaTypo03>Intygshistorik</IaTypo03>
-            <Table striped>
-              <thead>
-              <tr>
-                <th>Tid</th>
-                <th>Källa</th>
-                <th>Händelse</th>
-                <th></th>
-              </tr>
-              </thead>
-              <tbody>
-              {
-                intygInfo.events &&
-                intygInfo.events.map((row, index) => (<IntygEventRow key={index} event={row} fetchIntygInfo={fetchIntygInfo} />))
-              }
-              </tbody>
-            </Table>
-          </RowWrapper>
-
-          <RowWrapper>
-          <IaTypo03>Övrig intygsuppgifter</IaTypo03>
+        <RowWrapper>
+          <IaTypo03>Intygshistorik</IaTypo03>
           <Table striped>
+            <thead>
+            <tr>
+              <th>Tid</th>
+              <th>Källa</th>
+              <th>Händelse</th>
+              <th></th>
+            </tr>
+            </thead>
             <tbody>
-              <tr>
-                <TableTH>Signerat av</TableTH>
-                <TableTD>{intygInfo.signedByName}</TableTD>
-                {
-                  intygInfo.signedByEmail ?
-                    <TableTD>E-mail: {intygInfo.signedByEmail}</TableTD>
-                    :
-                    <TableTD>HSA-ID: {intygInfo.signedByHsaId}</TableTD>
-                }
-              </tr>
-              <tr>
-                <TableTH>Vårdenhet</TableTH>
-                <TableTD>{intygInfo.careUnitName}</TableTD>
-                <TableTD>HSA-ID: {intygInfo.careUnitHsaId}</TableTD>
-              </tr>
-              <tr>
-                <TableTH>Vårdgivare</TableTH>
-                <TableTD>{intygInfo.careGiverName}</TableTD>
-                <TableTD>HSA-ID: {intygInfo.careGiverHsaId}</TableTD>
-              </tr>
-              <tr>
-                <TableTH>Kompletteringsbegäran</TableTH>
-                <TableTD>
-                  <Kompletteringar intygInfo={intygInfo} />
-                </TableTD>
-                <TableTD />
-              </tr>
-              <tr>
-                <TableTH>Administrativa frågor</TableTH>
-                <TableTD><AdminQuestions intygInfo={intygInfo} /> </TableTD>
-                <TableTD />
-              </tr>
-              <tr>
-                <TableTH>Utfärdande enhet</TableTH>
-                <TableTD>{intygInfo.inWebcert ? 'Webcert' : 'Annan'}</TableTD>
-                <TableTD />
-              </tr>
+            {
+              intygInfo.events &&
+              intygInfo.events.map((row, index) => (<IntygEventRow key={index} event={row} fetchIntygInfo={fetchIntygInfo} />))
+            }
             </tbody>
           </Table>
-          </RowWrapper>
+        </RowWrapper>
 
-        </ModalBody>
-        <ModalFooter>
-          <Button id={'closeBtn'} onClick={handleClose} color={'default'}>
-            Stäng
-          </Button>
-        </ModalFooter>
-      </Modal>
+        <RowWrapper>
+          <IaTypo03>Övriga intygsuppgifter</IaTypo03>
+          <Table striped>
+            <tbody>
+            <tr>
+              <TableTH>Signerat av</TableTH>
+              <TableTD>{intygInfo.signedByName}</TableTD>
+              <TableTD>HSA-ID: {intygInfo.signedByHsaId}</TableTD>
+            </tr>
+            <tr>
+              <TableTH>Vårdenhet</TableTH>
+              <TableTD>{intygInfo.careUnitName}</TableTD>
+              <TableTD>HSA-ID: {intygInfo.careUnitHsaId}</TableTD>
+            </tr>
+            <tr>
+              <TableTH>Vårdgivare</TableTH>
+              <TableTD>{intygInfo.careGiverName}</TableTD>
+              <TableTD>HSA-ID: {intygInfo.careGiverHsaId}</TableTD>
+            </tr>
+            <tr>
+              <TableTH>Kompletteringsbegäran</TableTH>
+              <TableTD>
+                <Kompletteringar intygInfo={intygInfo} />
+              </TableTD>
+              <TableTD />
+            </tr>
+            <tr>
+              <TableTH>Administrativa frågor</TableTH>
+              <TableTD><AdminQuestions intygInfo={intygInfo} /> </TableTD>
+              <TableTD />
+            </tr>
+            <tr>
+              <TableTH>Utfärdande system</TableTH>
+              <TableTD>{intygInfo.inWebcert ? 'Webcert' : 'Annan'}</TableTD>
+              <TableTD />
+            </tr>
+            </tbody>
+          </Table>
+        </RowWrapper>
+
+      </ModalBody>
+      <ModalFooter>
+        <Button id={'closeBtn'} onClick={handleClose} color={'default'}>
+          Stäng
+        </Button>
+      </ModalFooter>
+    </Modal>
   )
 }
 
