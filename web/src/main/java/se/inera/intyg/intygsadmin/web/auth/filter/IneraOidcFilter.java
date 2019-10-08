7b4c927f6ee877e2a9774c8cd7a60c992c632cca
Martin Hesslund <martin.hesslund@callistaenterprise.se>
2019-10-08 11:12:39 +0200

    INTYGFV-10614: Samma hantering för fake och vanlig inloggning

--- web/src/main/java/se/inera/intyg/intygsadmin/web/auth/filter/IneraOidcFilter.java
+++ web/src/main/java/se/inera/intyg/intygsadmin/web/auth/filter/IneraOidcFilter.java
@@ -26,31 +26,23 @@ import com.nimbusds.oauth2.sdk.id.ClientID;
 import com.nimbusds.openid.connect.sdk.claims.IDTokenClaimsSet;
 import com.nimbusds.openid.connect.sdk.op.OIDCProviderMetadata;
 import com.nimbusds.openid.connect.sdk.validators.IDTokenValidator;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.UUID;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.client.OAuth2RestTemplate;
 import org.springframework.security.oauth2.common.OAuth2AccessToken;
 import org.springframework.security.oauth2.common.exceptions.OAuth2Exception;
-import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
-import se.inera.intyg.intygsadmin.persistence.entity.UserEntity;
 import se.inera.intyg.intygsadmin.persistence.service.UserPersistenceService;
 import se.inera.intyg.intygsadmin.web.auth.AuthenticationMethod;
-import se.inera.intyg.intygsadmin.web.auth.IntygsadminUser;
 import se.inera.intyg.intygsadmin.web.exception.IaAuthenticationException;
 import se.inera.intyg.intygsadmin.web.exception.IaErrorCode;
 
-public class IneraOidcFilter extends AbstractAuthenticationProcessingFilter {
+public class IneraOidcFilter extends BaseAuthenticationFilter {
 
     private static final Logger LOG = LoggerFactory.getLogger(IneraOidcFilter.class);
 
@@ -58,14 +50,11 @@ public class IneraOidcFilter extends AbstractAuthenticationProcessingFilter {
 
     private OAuth2RestTemplate restTemplate;
 
-    private UserPersistenceService userPersistenceService;
-
     public IneraOidcFilter(String defaultFilterProcessesUrl, OIDCProviderMetadata oidcProviderMetadata,
         String clientId, OAuth2RestTemplate restTemplate, UserPersistenceService userPersistenceService) {
-        super(defaultFilterProcessesUrl);
+        super(defaultFilterProcessesUrl, userPersistenceService);
         this.restTemplate = restTemplate;
-        this.userPersistenceService = userPersistenceService;
-        setAuthenticationManager(new NoopAuthenticationManager());
+
         try {
             idTokenValidator = new IDTokenValidator(oidcProviderMetadata.getIssuer(), new ClientID(clientId), JWSAlgorithm.RS256,
                 oidcProviderMetadata.getJWKSetURI().toURL());
@@ -89,20 +78,9 @@ public class IneraOidcFilter extends AbstractAuthenticationProcessingFilter {
             JWT jwtIdToken = JWTParser.parse(idTokenString);
             IDTokenClaimsSet idTokenClaimsSet = idTokenValidator.validate(jwtIdToken, null);
 
-            // CHECKSTYLE:OFF Indentation
             final String employeeHsaId = idTokenClaimsSet.getStringClaim("employeeHsaId");
-            UserEntity userEntity = userPersistenceService
-                .findByEmployeeHsaId(employeeHsaId).orElseThrow(
-                    () -> new BadCredentialsException(
-                        "Failed authentication. No IntygsadminUser for employeeHsaId " + employeeHsaId));
-            // CHECKSTYLE:ON Indentation
 
-            final IntygsadminUser user = new IntygsadminUser(userEntity, AuthenticationMethod.OIDC, accessToken);
-
-            List<SimpleGrantedAuthority> authorities = new ArrayList<>();
-            authorities.add(new SimpleGrantedAuthority(userEntity.getIntygsadminRole().name()));
-
-            return new UsernamePasswordAuthenticationToken(user, null, authorities);
+            return createAuthentication(employeeHsaId, AuthenticationMethod.OIDC, accessToken);
 
         } catch (final BadCredentialsException exception) {
             LOG.warn(exception.getMessage());
@@ -111,15 +89,5 @@ public class IneraOidcFilter extends AbstractAuthenticationProcessingFilter {
             LOG.error("Could not obtain user details from token", exception);
             throw new IaAuthenticationException(IaErrorCode.LOGIN_FEL001, exception.getMessage(), UUID.randomUUID().toString());
         }
-
-    }
-
-    private static class NoopAuthenticationManager implements AuthenticationManager {
-
-        @Override
-        public Authentication authenticate(Authentication authentication) throws AuthenticationException {
-            throw new UnsupportedOperationException("No authentication should be done with this AuthenticationManager");
-        }
-
     }
 }
