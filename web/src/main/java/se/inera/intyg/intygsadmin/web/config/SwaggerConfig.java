fa9f7485131e50698139fa8d7861529cdd61debe
Daniel Petersson <45845951+peterssond@users.noreply.github.com>
2020-03-20 09:37:41 +0100

    INTYGFV-12993: Updated Spring Boot version

--- web/src/main/java/se/inera/intyg/intygsadmin/web/config/SwaggerConfig.java
+++ web/src/main/java/se/inera/intyg/intygsadmin/web/config/SwaggerConfig.java
@@ -26,7 +26,6 @@ import java.lang.reflect.Type;
 import java.util.Arrays;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.core.Ordered;
 import org.springframework.data.domain.Pageable;
@@ -37,11 +36,12 @@ import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Profile("dev")
-@EnableSwagger2
-@Configuration
+// Removed Swagger due to bug and incompatability with newer Boot versions
+// https://github.com/springfox/springfox/issues/2932
+//@EnableSwagger2
+//@Configuration
 public class SwaggerConfig {
 
     private final TypeResolver typeResolver;
