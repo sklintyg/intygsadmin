a0c5e3433bb71df8536af0ea419784847f757713
Karl Friberg <28860194+IamFriberg@users.noreply.github.com>
2022-04-20 16:16:30 +0200

    Feature/intygfv 14393 fix return type (#140)
    * INTYGFV-14393: changed return type from getForObject to [] so Spring will map the object correctly.

---
+++ web/src/test/java/se/inera/intyg/intygsadmin/web/controller/DataExportControllerTest.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright (C) 2022 Inera AB (http://www.inera.se)
+ *
+ * This file is part of sklintyg (https://github.com/sklintyg).
+ *
+ * sklintyg is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * sklintyg is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package se.inera.intyg.intygsadmin.web.controller;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Pageable;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
+import se.inera.intyg.intygsadmin.web.controller.dto.CreateDataExportDTO;
+import se.inera.intyg.intygsadmin.web.integration.model.in.DataExportResponse;
+import se.inera.intyg.intygsadmin.web.service.IntygAvslutService;
+
+@ExtendWith(MockitoExtension.class)
+class DataExportControllerTest {
+
+    @Mock
+    private IntygAvslutService intygAvslutService;
+
+    @InjectMocks
+    private DataExportController dataExportController;
+
+    @Mock
+    private Pageable pageable;
+
+    @ParameterizedTest
+    @ValueSource(strings = {"createdAt", "creatorName", "terminationId", "status", "careProviderHsaId", "organizationNumber", "representativePersonId", "representativePhoneNumber"})
+    void listDataExports(String collumnName) {
+        Sort sort = Sort.by(Direction.DESC, collumnName);
+        when(pageable.getSort()).thenReturn(sort);
+        when(intygAvslutService.getDataExports()).thenReturn(new ArrayList<>());
+
+        assertNotNull(dataExportController.listDataExports(pageable));
+
+        verify(intygAvslutService, times(1)).getDataExports();
+    }
+
+    @Test
+    void createDataExport() {
+        CreateDataExportDTO createDataExportDTO = new CreateDataExportDTO();
+        when(intygAvslutService.createDataExport(createDataExportDTO)).thenReturn(new DataExportResponse());
+
+        assertNotNull(dataExportController.createDataExport(createDataExportDTO));
+
+        verify(intygAvslutService, times(1)).createDataExport(createDataExportDTO);
+    }
+}
