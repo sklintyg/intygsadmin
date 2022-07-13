fcc48861aa6f7754c2f03d7b0b53e1ab3131abf7
Matias Wurschmidt-Wang <matias.wurschmidt@soprasteria.com>
2022-07-13 08:44:43 +0200

    INTYGFV-14688: Remove unused import.

--- web/client/src/components/dataExport/dialogs/EraseDataExport.dialog.js
+++ web/client/src/components/dataExport/dialogs/EraseDataExport.dialog.js
@@ -1,4 +1,4 @@
-import React , { useState }  from 'react';
+import React from 'react';
 import { Button, Modal, ModalBody, ModalFooter, ModalHeader } from 'reactstrap';
 import modalContainer from '../../modalContainer/modalContainer';
 import { compose } from 'recompose';
