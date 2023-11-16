6b5d3d652f8ba80f8c7551b97c0045f937650358
Mikael Heglind <mikael.heglind@soprasteria.com>
2023-11-16 01:21:16 +0100

    INTYGFV-16055: Bumped versions and added some functionality moved from plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,16 @@
 import com.github.spotbugs.snom.SpotBugsPlugin
+import com.github.spotbugs.snom.SpotBugsTask
 import io.spring.gradle.dependencymanagement.DependencyManagementPlugin
+import se.inera.intyg.findGitRepository
+import se.inera.intyg.copyFile
 import se.inera.intyg.IntygPluginCheckstyleExtension
 import se.inera.intyg.JavaVersion
 import se.inera.intyg.TagReleaseTask
 import se.inera.intyg.intygsadmin.build.Config.Dependencies
 import se.inera.intyg.intygsadmin.build.Config.Jvm
 import se.inera.intyg.intygsadmin.build.Config.TestDependencies
+import java.nio.file.Files
+import java.nio.file.Paths
 
 plugins {
   id("se.inera.intyg.plugin.common") apply false
@@ -29,7 +34,7 @@ allprojects {
   }
 
   extra.apply {
-    set("intygInfraVersion", System.getProperty("infraVersion", "3.20.0-SNAPSHOT"))
+    set("intygInfraVersion", System.getProperty("infraVersion", "3.21.0-SNAPSHOT"))
     set("errorproneExclude", "true") //FIXME: Errorprone does not support Kotlin and KAPT. Until it does this will exclude the errorprone task for this project
   }
 
@@ -44,15 +49,25 @@ allprojects {
   }
 
   if (project.hasProperty("codeQuality")) {
-    dependencies {
-      SpotBugsPlugin.SLF4J_CONFIG_NAME("org.slf4j:slf4j-simple")
-    }
-  }
+    tasks {
+      val spotbugsIncludesAndExcludes by creating {
+        dependsOn(":unzipBuildTools")
+        val spotbugsToolDir = "${rootProject.layout.buildDirectory.get().asFile}/build-tools/spotbugs"
+        withType<SpotBugsTask> {
+          includeFilter.set(file("${spotbugsToolDir}/spotbugsIncludeFilter.xml"))
+          excludeFilter.set(file("${spotbugsToolDir}/spotbugsExcludeFilter.xml"))
+        }
+      }
 
-  tasks.clean {
-    setDelete(fileTree(project.layout.buildDirectory.get().asFile) {
-      exclude("/tmp/.cache/expanded/expanded.lock")
-    })
+      withType<Checkstyle> { named("checkstyleMain") { dependsOn(":unzipBuildTools") } }
+      withType<JacocoReport> { named("jacocoTestReport") { mustRunAfter(":unzipBuildTools") } }
+      withType<SpotBugsTask> {
+        named("spotbugsMain") { dependsOn(spotbugsIncludesAndExcludes) }
+        named("spotbugsTest") { enabled = false }
+      }
+    }
+
+    dependencies { SpotBugsPlugin.SLF4J_CONFIG_NAME("org.slf4j:slf4j-simple") }
   }
 }
 
@@ -91,8 +106,33 @@ subprojects {
   }
 }
 
-tasks.register<TagReleaseTask>("tagRelease")
-
-subprojects.map {it.name }.forEach {
+subprojects.map { it.name }.forEach {
   dependencies { testReportAggregation(project(":${it}")) }
+}
+
+tasks {
+  register("tagRelease", TagReleaseTask::class)
+
+  val unzipBuildTools by registering(Copy::class) {
+    into("${rootProject.layout.buildDirectory.get().asFile}/build-tools/")
+    from(rootProject.buildscript.configurations.getByName("classpath").find { it.name.contains("se.inera.intyg.plugin.common") }
+            ?.let { zipTree(it).matching { include("/spotbugs/**", "/git_hooks/**") } })
+  }
+
+  val applyGitHooks by registering(Copy::class) {
+    dependsOn(unzipBuildTools)
+    val repository = findGitRepository(project.rootProject.projectDir)
+    val gitHooksDir = "${rootProject.layout.buildDirectory.get().asFile}/build-tools/git_hooks"
+    val commitMsg = file("${gitHooksDir}/commit-msg")
+    val preCommit = file("${gitHooksDir}/pre-commit")
+    val toDir = Paths.get(repository.directory.path, "hooks")
+
+    if (!Files.exists(toDir)) {
+      Files.createDirectory(toDir)
+    }
+    copyFile(commitMsg, toDir)
+    copyFile(preCommit, toDir)
+  }
+
+  compileJava { dependsOn(applyGitHooks) }
 }
