64fbc2ef6fda9297a486674cf47a75ca76506e91
mhornfeldt <michaela.hoernfeldt@soprasteria.com>
2025-02-14 08:11:36 +0100

    K1J-771: Added pojo

--- web/src/main/java/se/inera/intyg/intygsadmin/web/controller/dto/CountStatusesForCareGiverRequestDTO.java
+++ web/src/main/java/se/inera/intyg/intygsadmin/web/controller/dto/CountStatusesForCareGiverRequestDTO.java
@@ -2,12 +2,15 @@ package se.inera.intyg.intygsadmin.web.controller.dto;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonPOJOBuilder;
 import lombok.Builder;
 import lombok.Value;
 import se.inera.intyg.intygsadmin.web.service.status.NotificationStatusEnum;
 
 @Value
 @Builder
+@JsonDeserialize(builder = CountStatusesForCareGiverRequestDTO.CountStatusesForCareGiverRequestDTOBuilder.class)
 public class CountStatusesForCareGiverRequestDTO {
 
     String careGiverId;
@@ -15,4 +18,7 @@ public class CountStatusesForCareGiverRequestDTO {
     LocalDateTime end;
     List<NotificationStatusEnum> status;
 
+    @JsonPOJOBuilder(withPrefix = "")
+    public static class CountStatusesForCareGiverRequestDTOBuilder {
+    }
 }
