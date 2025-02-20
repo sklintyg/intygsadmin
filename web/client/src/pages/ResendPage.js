22d1acb2039171f2af0d6cd02b11c22e0ae9431a
Samuel Tilly <28404+SamuelTilly@users.noreply.github.com>
2025-02-20 07:57:12 +0100

    K1J-428: Fix linting issue that is blocking build process (#185)
    * K1J-428: Fix linting issue that is blocking build process
    
    * cleanup gitignore

--- web/client/src/pages/ResendPage.js
+++ web/client/src/pages/ResendPage.js
@@ -1,10 +1,8 @@
-import React from 'react'
-import { CustomScrollingContainer, FlexColumnContainer, PageContainer } from '../components/styles/iaLayout'
 import MenuBar from '../components/iaMenu/MenuBar'
-import DataExport from '../components/dataExport/DataExport'
 import PageHeader from '../components/styles/PageHeader'
-import { DocIcon } from '../components/styles/iaSvgIcons'
 import IaColors from '../components/styles/iaColors'
+import { CustomScrollingContainer, FlexColumnContainer, PageContainer } from '../components/styles/iaLayout'
+import { DocIcon } from '../components/styles/iaSvgIcons'
 
 const ResendPage = () => {
   return (
@@ -16,9 +14,7 @@ const ResendPage = () => {
           subHeader="Här kan du skapa en omsändning av händelser för både enskilda intygs-id och för hela vårdgivare/vårdenheter. Administrerade omsändningar visas i tabellen."
           icon={<DocIcon color={IaColors.IA_COLOR_02} />}
         />
-        <PageContainer>
-          {/* TODO: Add page content */}
-        </PageContainer>
+        <PageContainer>{/* TODO: Add page content */}</PageContainer>
       </CustomScrollingContainer>
     </FlexColumnContainer>
   )
